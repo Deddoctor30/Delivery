33c1bc1aa4ac7f925af418a2d15d5d6367c09aa9
Wadim <alinsbomb@yandex.ru>
2026-09-10 11:21:53 +0300

    feat(backend): static files serving

--- 03-backend-nestjs/src/main.ts
+++ 03-backend-nestjs/src/main.ts
@@ -1,8 +1,10 @@
 import helmet from '@fastify/helmet';
+import fastifyStatic from '@fastify/static';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { join } from 'node:path';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './core/filters/all-exceptions.filter';
 import { LoggingInterceptor } from './core/interceptors/logging.interceptor';
@@ -13,6 +15,11 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   await app.register(helmet);
+
+  await app.register(fastifyStatic, {
+    root: join(__dirname, '..', '..', 'static'), // dist/src/main.js → ../../static
+    prefix: '/static/',
+  });
 
   const corsOrigins = configService.get<string>('CORS_ORIGINS', '');
   app.enableCors({
